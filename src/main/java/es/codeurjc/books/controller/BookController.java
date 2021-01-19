45d6b44163c2ae06fbb9c4f891a9f06ed8b97d61
joaquin <jdevicente@genasys.com>
2021-01-19 05:09:52 +0100

    Added books endpoints

--- src/main/java/es/codeurjc/books/controller/BookController.java
+++ src/main/java/es/codeurjc/books/controller/BookController.java
@@ -6,10 +6,13 @@ import java.net.URI;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,5 +57,33 @@ public class BookController {
 
 		return ResponseEntity.created(location).body(book);
 	}
+	
+	@PutMapping("/books/{id}")
+	@JsonView(Book.Basic.class)
+	public ResponseEntity<Book> updateBook(@RequestBody Book book, @PathVariable long id) {
+		
+		if (books.findById(id).isPresent()) {
+			books.deleteById(id);
+			books.save(book);
+			URI location = fromCurrentRequest().path("/{id}")
+				.buildAndExpand(book.getId()).toUri();
+
+			return ResponseEntity.created(location).body(book);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@DeleteMapping("/books/{id}")
+	@JsonView(BookWithCommentsAndUser.class)
+	public ResponseEntity<HttpStatus> deleteBook(@PathVariable long id) {
+
+		if (books.findById(id).isPresent()) {		
+			books.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
 
 }
