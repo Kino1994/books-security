6819c1cf0ac3a21b6ea28ac442f64cd5f92622a5
joaquin <jdevicente@genasys.com>
2021-01-19 04:44:27 +0100

    Fix permissions

--- src/main/java/es/codeurjc/books/security/Constants.java
+++ src/main/java/es/codeurjc/books/security/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	// Spring Security
 
 	public static final String LOGIN_URL = "/login";
-	public static final String BOOKS_URL = "/books";
+	public static final String BOOKS_URL = "/books/";
 	public static final String USERS_URL = "/users";
 	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
